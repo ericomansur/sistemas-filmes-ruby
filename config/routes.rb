b960c8e0915871655a8b9690342d948ca1774879
Eric <ericmansur13@gmail.com>
2026-02-07 22:40:35 -0300

    adicionando Assistir mais tarde e Gerar Novas

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resource :preferences, only: [:edit, :update]
   
   # Recomendações
-  resources :recommendations, except: [:new, :create, :edit, :update] do
+  resources :recommendations, only: [:index, :show, :destroy] do
     member do
       patch :mark_watched
     end
@@ -19,6 +19,13 @@ Rails.application.routes.draw do
   
   # Rota especial para gerar recomendações (FORA do resources)
   post 'recommendations/generate', to: 'recommendations#generate', as: 'generate_recommendations'
+  
+  # Assistir Mais Tarde
+  resources :watch_laters, only: [:index, :create, :destroy] do
+    collection do
+      post :add_from_tmdb
+    end
+  end
 
   # Health check
   get "up" => "rails/health#show", as: :rails_health_check
